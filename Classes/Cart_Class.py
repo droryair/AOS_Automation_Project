0239d7ceb43cd4b564b5b4330915d66c9a4b2229
droryair <drorya2324@gmail.com>
2021-11-15 11:21:12 +0200

    finished Cart class

--- Classes/Cart_Class.py
+++ Classes/Cart_Class.py
@@ -3,6 +3,98 @@ cart (quantity, name, color, price, remove, "shopping cart" text, Total)
 """
 from selenium.webdriver.common.by import By
 
+
 class Cart:
     def __init__(self, driver):
         self.driver = driver
+        self.table_rows = self.driver.find_elements(By.CSS_SELECTOR,"div[id=shoppingCart]>table>tbody>tr")
+
+## == inner-class functions == ##
+
+    # this function will receive a string and turn it into a number (int/float)
+    # !!! move to the main file? what if we want the string and not the number itself? !!! #
+    '''    
+    def str_to_num(self, stri: str):
+        """
+        :param stri: string including numeric characters and no more than one dot (.)
+        :return: that string as a number type (int/float), depends on the existence of a dot (.)
+        """
+        is_float = False
+        for char in stri:
+            if not char.isnumeric():
+                if char == '.':
+                    is_float = True
+                    continue
+                stri = stri.replace(char, '')
+        if is_float:
+            return float(stri)
+        return int(stri)
+    '''
+
+    # this function will receive an index, and will return a list including the column matching to the index.
+    def get_list_cells_elems(self, index: int):
+        """
+        :param index: an index matching a wanted column in the cart table.
+                      (for example: 'quantities' column's index is 4)
+        :return: a list including all of the cells' ELEMENTS from the column matching the index
+        """
+        lst = []
+        for row in self.table_rows:
+            row_cells = row.find_elements(By.TAG_NAME, 'td')
+            lst.append(row_cells[index])
+        return lst
+
+##  ===
+
+    # returns a list of all of the products' names in the cart
+    def get_products_names(self):
+        """
+        :return: a list of all of the products' names in the cart
+                (index 0 = top product's name)
+        """
+        names_elems = self.get_list_cells_elems(1)
+        names = []
+        for name_elem in names_elems:
+            names.append(name_elem.text)
+        return names
+
+    # returns a list of all of the products' quantities in the cart
+    def get_products_quantities(self):
+        """
+        :return: a list of all of the products' quantities in the cart
+                (index 0 = top product's quantity)
+        """
+        quantities_elems = self.get_list_cells_elems(4)
+        quantities = []
+        for quant_elem in quantities_elems:
+            quantities.append(quant_elem.text)
+            # quantities.append(self.str_to_num(quant_elem.text))
+        return quantities
+
+    # returns a list of all of the products' colors in the cart
+    def get_products_colors(self):
+        """
+        :return: a list of all of the products' colors in the cart
+                (index 0 = top product's color)
+        """
+        colors_tds = self.get_list_cells_elems(3)
+        colors = []
+        for color_td in colors_tds:
+            colors.append(color_td.find_element(By.TAG_NAME, "span").get_attribute("title"))
+        return colors
+
+    # returns a list of all of the products' prices in the cart
+    def get_products_price(self):
+        """
+        :return: a list of all of the products' prices in the cart
+                (index 0 = top product's price)
+        """
+        prices_tds = self.get_list_cells_elems(5)
+        prices = []
+        for price_td in prices_tds:
+            price_str = price_td.find_element(By.TAG_NAME, 'p').text
+            prices.append(price_str)
+            # prices.append(self.str_to_num(price_str))
+        return prices
+
+
--- Project_Main.py
+++ Project_Main.py
@@ -3,6 +3,7 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
+from Classes.Cart_Class import Cart
 from Classes.Category_Class import Category
 from Classes.Homepage_Class import Homepage
 from Classes.Product_Class import Product
@@ -13,6 +14,26 @@ driver = webdriver.Chrome(service=service)
 driver.get("https://www.advantageonlineshopping.com/#/")
 
 driver.implicitly_wait(10)
+driver.maximize_window()
+
+
+# this function will receive a string and turn it into a number (int/float)
+def str_to_num(stri: str):
+    """
+    :param stri: string including numeric characters and no more than one dot (.)
+    :return: that string as a number type (int/float), depends on the existence of a dot (.)
+    """
+    is_float = False
+    for char in stri:
+        if not char.isnumeric():
+            if char == '.':
+                is_float = True
+                continue
+            stri = stri.replace(char, '')
+    if is_float:
+        return float(stri)
+    return int(stri)
+
 
 homepage = Homepage(driver)
 sleep(10)
@@ -23,6 +44,7 @@ sleep(10)
 category.click_product()
 
 product = Product(driver)
+sleep(1)
 product.change_quantity(2)
 product.add_to_cart()
 
@@ -36,6 +58,25 @@ sleep(1)
 product.change_quantity(3)
 product.add_to_cart()
 topbar.click_cart()
+
+cart = Cart(driver)
+sleep(3)
+quantities_str = cart.get_products_quantities()
+print("quantities: ", quantities_str)
+names = cart.get_products_names()
+print("names: ", names)
+colors = cart.get_products_colors()
+print("colors: ", colors)
+prices_str = cart.get_products_price()
+prices_num = []
+for price_str in prices_str:
+    num_price = str_to_num(price_str)
+    prices_num.append(num_price)
+    print("type num_price", type(num_price))
+print("prices_str: ", prices_str)
+print("prices_num: ", prices_num)
+
+
 
 
 
